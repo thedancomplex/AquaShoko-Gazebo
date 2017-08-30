b159850d317f36e4ff7d189ca6aaad95f5818d31
RobobAlberto <albertopnx@gmail.com>
2017-08-30 00:53:03 -0400

    build added for robocontrol, contains worlds for simulation

---
+++ build/CMakeFiles/roboControl.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/albertoperez/GazeboAquaShoko/roboControl.cc" "/Users/albertoperez/GazeboAquaShoko/build/CMakeFiles/roboControl.dir/roboControl.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LIBBULLET_VERSION=2.86"
+  "LIBBULLET_VERSION_GT_282"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/gazebo8/8.1.1_1/include"
+  "/usr/local/Cellar/gazebo8/8.1.1_1/include/gazebo-8"
+  "/usr/local/Cellar/bullet/2.86.1_1/include/bullet"
+  "/usr/local/Cellar/bullet/2.86.1_1/include"
+  "/usr/local/Cellar/simbody/3.5.4_1/include/simbody"
+  "/usr/local/include"
+  "/usr/local/Cellar/sdformat5/5.1.0_1/include/sdformat-5.1"
+  "/usr/local/include/ignition/math3"
+  "/usr/local/Cellar/ogre1.9/1.9-20160714-108ab0bcc69603dba32c0ffd4bbbc39051f421c9_2/include/OGRE"
+  "/usr/local/Cellar/ogre1.9/1.9-20160714-108ab0bcc69603dba32c0ffd4bbbc39051f421c9_2/include"
+  "/usr/local/Cellar/ogre1.9/1.9-20160714-108ab0bcc69603dba32c0ffd4bbbc39051f421c9_2/include/OGRE/Terrain"
+  "/usr/local/Cellar/ogre1.9/1.9-20160714-108ab0bcc69603dba32c0ffd4bbbc39051f421c9_2/include/OGRE/Paging"
+  "/usr/local/Cellar/ignition-transport3/3.0.1_4/include/ignition/transport3"
+  "/usr/local/Cellar/zeromq/4.2.2/include"
+  "/usr/local/Cellar/ossp-uuid/1.6.2_2/include/ossp"
+  "/usr/local/include/ignition/msgs0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/roboControl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/roboControl.dir/roboControl.cc.o"
+  "libroboControl.pdb"
+  "libroboControl.dylib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/roboControl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
